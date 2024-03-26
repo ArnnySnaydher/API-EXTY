0cdc5f759c738139b7eedd38e3620e4e0a93e92f
Aryans <quispeloayza18@hotmail.com>
2024-03-26 18:06:47 -0500

    Eslint

--- build/index.js
+++ build/index.js
@@ -4,8 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diaries_1 = __importDefault(require("./routes/diaries"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json()); //middleware que transforma la req.body a un json 
-const port = 5000;
-app.get('/', (_req, res) => res.send('Hello World!'));
+app.use(express_1.default.json()); //  middleware que transforma la req.body a un json
+const port = 3000;
+app.get('/', (_req, res) => res.send('Hello World AS!'));
+//  En la ruta usar diaryRouter
+app.use('/api/diaries', diaries_1.default);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
